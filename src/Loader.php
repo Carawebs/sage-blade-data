aa985a25c132bef8ff4d00d5dbf84c7ebe715c55
David Egan <david@carawebs.com>
2017-03-28 21:33:18 +0100

    Refactor so that controller child-classes use a common data object.

--- src/Loader.php
+++ src/Loader.php
@@ -2,14 +2,24 @@
 namespace Carawebs\SageBladeData;
 
 use Carawebs\SageBladeData\Filters;
+
 /**
-*
-*/
+ * Class to instantiate controllers.
+ *
+ * Note that by default the path to the controllers directory is
+ * `get_template_directory() . '/src/Carawebs/Controllers'`. This is filtered
+ * by 'carawebs-controllers/path-to-controllers'.
+ */
 class Loader
 {
-
+    /**
+     * Set necessary data.
+     * @param Filters $filters      Object that controls hooking data to 'template_include'.
+     * @param Object  $dataAccessor (Usually) an object used to access post metadata.
+     */
     function __construct(Filters $filters, $dataAccessor = NULL)
     {
+        $this->dataObject = new CommonData;
         $this->dataAccessor = $dataAccessor;
         $this->filters = $filters;
         $this->setControllerFiles();
@@ -17,6 +27,14 @@ class Loader
         $this->loadAll();
     }
 
+    /**
+     * Set an array of paths to the Controller classes.
+     *
+     * Controllers extend the Controller abstract class and are usually located
+     * within the WordPress theme. Default path to controllers is:
+     * `get_template_directory() . '/src/Carawebs/Controllers'`.
+     * Filtered by: 'carawebs-controllers/path-to-controllers'.
+     */
     public function setControllerFiles()
     {
         $pathToControllers = apply_filters('carawebs-controllers/path-to-controllers', get_template_directory() . '/src/Carawebs/Controllers');
@@ -24,11 +42,13 @@ class Loader
         $this->controllerFiles = glob($dir);
     }
 
+    /**
+     * Create an array of fully-namespaced controller class names.
+     */
     public function setControllerClasses()
     {
         if (empty($this->controllerFiles)) return;
         $classes = [];
-
         foreach($this->controllerFiles as $file)
         {
             if(!is_dir($file)) {
@@ -39,13 +59,22 @@ class Loader
             }
         }
         $this->classes = $classes;
+        var_dump($this->classes);
     }
 
+    /**
+     * Instantiate all controller classes.
+     *
+     * As controller objects are created, specific data is set up and hooked to
+     * 'carawebs/template/{$templateClass}/data'. This is then used to send data
+     * to templates in the `$this->filters->init()` method.
+     * @return void
+     */
     public function loadAll()
     {
         if (empty($this->controllerFiles)) return;
         foreach ($this->classes as $class) {
-            new $class($this->dataAccessor);
+            new $class($this->dataObject, $this->dataAccessor);
         }
         $this->filters->init();
     }
